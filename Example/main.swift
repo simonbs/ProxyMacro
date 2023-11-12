73642d778f26d21b57ad17ade4e81f86e7a5fd38
Simon Støvring <mail@simonbs.dk>
2023-11-12 23:13:50 +0100

    Improves example

--- Example/main.swift
+++ Example/main.swift
@@ -1,14 +1,14 @@
 import ProxyMacro
 
-final class StateStore {
-    var number = 42 {
-        didSet {
-            print("Did change value in \(type(of: self)) from \(oldValue) to \(number)")
+final class MyObj {
+    private final class StateStore {
+        var number = 42 {
+            didSet {
+                print("Did change value in \(type(of: self)) from \(oldValue) to \(number)")
+            }
         }
     }
-}
 
-final class MyObj {
     @Proxy(\Self.stateStore.number)
     var number: Int
 
